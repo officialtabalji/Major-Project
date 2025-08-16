d5a9f0a708a2e96f90321d70e652dc0e074a3e81
officialtabalji <shreyamajik123@gmail.com>
2025-08-16 23:50:07 +0530

    login added

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express=require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync= require("../utils/wrapAsync");
+const passport=require("passport");
 
 router.get("/signup",(req,res) => {
     res.render("users/signup.ejs");
@@ -21,5 +22,14 @@ router.post("/signup", wrapAsync(async(req,res)=>{
     }
    
 }));
+
+router.get("/login",(req,res)=>{
+    res.render("users/login.ejs");
+});
+
+router.post("/login", passport.authenticate("local",{ failureRedirect: '/login', failureFlash: true}), async(req,res)=>{
+    req.flash("success","Welcome to Wanderlust,You're logged in!");
+    res.redirect("/listings");
+});
 
 module.exports=router;
