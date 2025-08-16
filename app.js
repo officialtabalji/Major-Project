21be5a7060d8b9d3001aaa0d6b037f3dc2babc68
officialtabalji <shreyamajik123@gmail.com>
2025-08-16 12:31:30 +0530

    initial commit

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const mongoose= require("mongoose");
 const path=require("path");
 const methodOverride= require("method-override");
 const ejsMate=require("ejs-mate");
-const Listing = require("./models/listing");
-const wrapAsync=require("./utils/wrapAsync.js");
 const ExpressError=require("./utils/ExpressError.js");
-const {listingSchema}= require("./schema.js");
+const flash=require("connect-flash");
+const session=require("express-session");
+const passport=require("passport");
+const LocalStrategy=require("passport-local");
+const User=require("./models/user.js");
 
+
+
+
+const listingRouter= require("./routes/listing.js");
+const reviewRouter=require("./routes/review.js");
+const userRouter=require("./routes/user.js");
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
@@ -32,67 +40,52 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
+const sessionOptions={
+    secret: "mysupersecretcode",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires:Date.now() + 7 *24 * 60 *60 *1000,
+        maxage: 7 *24 * 60 *60 *1000,
+        httpOnly: true,
+    },
+};
+
 app.get("/",(req,res)=>{
     res.send("Hi,I am Root");
 });
 
-const validateListing= (req,res,next)=>{
-    let {error}=listingSchema.validate(req.body);
-    if(error){
-        let errMsg=error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errMsg);
-    }else{
-        next();
-    }
 
-}
+app.use(session(sessionOptions));
+app.use(flash());
 
-//Index Route
-app.get("/listings", wrapAsync(async (req,res)=>{
-    const allListings=await Listing.find({});
-    res.render("listings/index.ejs",{allListings});
-}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
 
-//New Route 
-app.get("/listings/new", (req,res) =>{
-    res.render("listings/new.ejs");
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser()); 
+
+app.use((req,res,next)=>{
+    res.locals.success=req.flash("success");
+    res.locals.error=req.flash("error");
+    next();
 });
 
-//Show Route 
-app.get("/listings/:id", wrapAsync(async (req,res) => {
-    let{id}=req.params;
-    const listing= await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
-}));
+app.get("/demouser",async (req, res) => {
+    let fakeUser= new User({
+        email: "student@gmail.com",
+        username: "delta-student",
+    });
 
-//Create Route
-app.post("/listings", validateListing, wrapAsync(async (req, res, next) => {
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-}));
+    let registeredUSer= await User.register(fakeUser,"helloworld");
+    res.send(registeredUser);
+});
 
+app.use("/listings",listingRouter);
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/",userRouter);
 
-//Edit Route
-app.get("/listings/:id/edit", wrapAsync(async(req,res)=>{
-    let{id}=req.params;
-    const listing= await Listing.findById(id);
-    res.render("listings/edit.ejs",{listing});
-}));
-//Update Route
-app.put("/listings/:id",validateListing, wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    res.redirect(`/listings/${id}`);
-}));
-
-//Delete Route 
-app.delete("/listings/:id", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-}));
 
 app.all("*",(req,res,next)=> {
     next(new ExpressError(404,"Page not Found!"));
